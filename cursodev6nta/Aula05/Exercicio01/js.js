b3d1c19ca098b22cbf9762b961689e166a53a079
26007345 <26007345@UNA007-L201-003.serlab.intra>
2018-09-13 17:50:52 -0300

    Aula5Exercicio1

---
+++ cursodev6nta/Aula05/Exercicio01/js.js
@@ -0,0 +1,30 @@
+function soma() {
+	var numero = parseFloat(document.getElementById("nume1").value);
+	var numero1 = parseFloat(document.getElementById("nume2").value);
+	document.getElementById("nome").innerHTML = numero + numero1;
+	
+	
+} 
+function subtrair() {
+	var numero = parseFloat(document.getElementById("nume1").value);
+	var numero1 = parseFloat(document.getElementById("nume2").value);
+	document.getElementById("nome").innerHTML = numero - numero1;
+	
+	
+	
+} 	
+function multiplicacao() {
+	var numero = parseFloat(document.getElementById("nume1").value);
+	var numero1 = parseFloat(document.getElementById("nume2").value);
+	document.getElementById("nome").innerHTML = numero * numero1;
+	
+	
+} 	
+function divisao() {
+	var numero = parseFloat(document.getElementById("nume1").value);
+	var numero1 = parseFloat(document.getElementById("nume2").value);
+	document.getElementById("nome").innerHTML = numero / numero1;
+	
+	
+	
+} 	
